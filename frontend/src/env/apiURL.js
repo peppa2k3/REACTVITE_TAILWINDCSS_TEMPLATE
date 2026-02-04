39c50331acee8dce6ed8e4a8a8a177c121103483
unknown <hai.dang@vnpd.com>
2026-02-04 09:43:54 +0700

    fix(cicd): fix env FE in build no meta.env

--- frontend/src/env/apiURL.js
+++ frontend/src/env/apiURL.js
@@ -1,10 +1,16 @@
 // build env values
-const URL_Socket = import.meta.env.VITE_API_BASE_URL || 'https://api.dangngochai.io.vn';
+const URL_Socket = 'https://api.dangngochai.io.vn';
 const URL_recommendSystem = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000/';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.dangngochai.io.vn';
+const API_BASE_URL = 'https://api.dangngochai.io.vn';
 
 export { API_BASE_URL, URL_Socket, URL_recommendSystem };
-//import.meta.env.VITE_API_BASE_URL || "https://hai2806.pythonanywhere.com/";
-// export const socket = io(URL);
 //RESTFUl API backend
+
+//local api
+// const URL_Socket = import.meta.env.VITE_API_BASE_URL || 'https://api.dangngochai.io.vn';
+// const URL_recommendSystem = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000/';
+
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.dangngochai.io.vn';
+
+// export { API_BASE_URL, URL_Socket, URL_recommendSystem };
